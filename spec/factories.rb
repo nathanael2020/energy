4c8ce4f3edf91bd644973cd8babbf76478fdc00e
Nathanael Miksis <nathanael.miksis@gmail.com>
2012-12-30 18:21:35 -0800

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Nathanael Miksis"
+    email    "nathanael@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
